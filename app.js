a3fd3cd014a305d52ce4493de8b66accf16a4ab6
jericam832 <jericam32@gmail.com>
2019-10-24 12:20:34 -0700

    some style

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ function handleSubmit(event) {
   fade();
   themeAudio.play();
   document.getElementById('overlay').style.visibility = 'hidden';
-  document.getElementById('namedata').textContent = `Username: ${userName}`;
+  document.getElementById('namedata').textContent = `Player: ${userName}`;
 }
 function fade() {
   document.getElementById('overlay').style.opacity = '0';
